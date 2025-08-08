8114bad284180980d6cc8e1206b45f931e5fc037
Abdurozzaq <abdurozzaq00@gmail.com>
2025-08-09 02:18:50 +0530

    Fix BUG

--- resources/js/pages/Subscription/Info.vue
+++ resources/js/pages/Subscription/Info.vue
@@ -1,5 +1,5 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
+import AppLayout from '@/layouts/AppLayout.vue';
 import { Head, Link } from '@inertiajs/vue3';
 import { defineProps } from 'vue';
 
