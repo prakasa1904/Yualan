cb7b65e6344b7eabb81a5d7b4eb6c85b31143862
root <root@yualan.web.id>
2025-08-08 20:00:20 +0530

    Fix Deployment Test

--- database/migrations/2025_07_11_090406_create_users_table.php
+++ database/migrations/2025_07_11_090406_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->uuid('tenant_id'); // Foreign key to tenants table
+            $table->uuid('tenant_id')->nullable(); // Foreign key to tenants table
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
