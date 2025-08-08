a9dec2ed4b9178f616fe1788655e2df1944017a9
Abdurozzaq Nurul Hadi <abdurozzaq00@gmail.com>
2025-08-09 02:32:06 +0700

    Update Login Button Color

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -89,7 +89,7 @@ const features = [
                 <template v-else>
                     <Link
                         :href="route('login')"
-                        class="px-4 py-2 rounded-md text-sm font-medium text-blue-600 hover:text-blue-700 transition-colors duration-200"
+                        class="px-4 py-2 rounded-md text-sm font-medium bg-white text-blue-600 hover:text-blue-700 transition-colors duration-200"
                     >
                         Login
                     </Link>
